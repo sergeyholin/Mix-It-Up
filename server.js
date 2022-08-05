84996bb90e182510cd3d7285d9ade63d8a1aec51
Sergey Holin <holin_sergey@yahoo.com>
2022-08-05 12:55:31 -0700

    added handlebars to package.json

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+// Express requires
 const express = require("express")
 const routes = require('./routes');
+// Handlebars requires
+const exphbs = require('express-handlebars');
+const path = require('path');
+const hbs = exphbs.create({});
 
+// Express dependencies
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+// Express Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
+// Express rerouting
 app.use(routes);
 
+// Start server to egin listening
 app.listen(PORT, () => {
 console.log(`App listening on port ${PORT}!`);
 });
-
-
