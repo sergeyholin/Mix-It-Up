2e3961159ab67118b36bdb9e096d62e9dc70568c
Sergey Holin <holin_sergey@yahoo.com>
2022-08-06 21:53:31 -0700

    Update

--- controllers/api/displayAll.js
+++ controllers/api/displayAll.js
@@ -1,15 +1,7 @@
 const router = require('express').Router();
 const { Cocktail } = require('../../models');
 
-// Get all cocktails from the db Route.
-// router.get('/display_all', async (req, res) => {
-//     const cocktailData = await Cocktail.findAll().catch((err) => { 
-//         res.json(err);
-//       });
-//         const cocktails = cocktailData.map((cocktail) => cocktail.get({ plain: true }));
-//         res.render('all', { cocktails });
-//       });
-
+// Get all cocktails from the database Route.
 router.get('/', async (req, res) => {
   const cocktailData = await Cocktail.findAll().catch((err) => { 
       res.json(err);
